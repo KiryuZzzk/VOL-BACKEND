b9cda9d3e79cf4b19d2593566494b53b10192910
Kiryu <kiryuzrk@gmail.com>
2025-07-10 15:45:41 -0600

    Cambio en public.routes

--- routes/public.routes.js
+++ routes/public.routes.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const publicController = require("../controllers/public.controller");
+const { authMiddleware } = require("../middlewares/auth");
 
 router.post("/register", publicController.registerUser);
-router.get("/validar-usuario", publicController.validarUsuario);
+router.get("/validar-usuario", authMiddleware, publicController.validarUsuario);
 
 module.exports = router;
