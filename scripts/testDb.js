e17ebc95f257a379e76d068595bee85fb640eae6
Kiryu <kiryuzrk@gmail.com>
2025-12-30 11:17:15 -0600

    Cambiando config

--- scripts/testDb.js
+++
@@ -1,9 +0,0 @@
-const db = require("../config/db");
-
-(async () => {
-  const conn = await db.getConnection();
-  const [rows] = await conn.query("SELECT 1 as ok");
-  console.log(rows);
-  conn.release();
-  await db.end();
-})();
